fa30cec587d838d6243aaf0c226f00e9ca7a4d0e
Maximelian Mumladze <Makcieml@gmail.com>
2019-10-21 19:31:56 +0300

    asserts

---
+++ include/shush-file.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "shush-dump.hpp"
+#include "io.h"
+
+namespace shush {
+namespace file {
+
+inline const size_t MAXIMUM_CHARS_PER_WRITE = 10000;
+inline const size_t MAXIMUM_FILE_NAME       = 50;
+
+enum Errc {
+  COULD_NOT_OPEN_FILE      = 0,
+  LOST_ACCESS_TO_A_FILE    = 1,
+  COULD_NOT_READ_FROM_FILE = 2,
+  NO_FILE_NAME_GIVEN       = 3,
+  COULD_NOT_WRITE_TO_FILE  = 4
+};
+
+class File {
+public:
+  File(const char* file_name, const char* mode);
+
+  void Write(const char* str);
+  void Read(char* out_str, size_t count);
+  size_t GetFileSize();
+
+private:
+  void Ok();
+  char* GetDumpMessage(int error_code);
+  char* GetErrorName(int error_code);
+
+  FILE* file_;
+  char file_name_[MAXIMUM_FILE_NAME];
+};
+
+// TODO this implies that only one file will throw an exception
+// how about others?
+static char error_name_container[MAXIMUM_FILE_NAME] {};
+static char dump_msg_container[MAXIMUM_FILE_NAME]   {};
+
+}
+}
---
+++ src/shush-file.cpp
@@ -0,0 +1,74 @@
+#include "shush-file.hpp"
+
+
+shush::file::File::File(const char* file_name, const char* mode) {
+  UMASSERT(!file_name, NO_FILE_NAME_GIVEN);
+  UMASSERT(strlen(file_name), NO_FILE_NAME_GIVEN);
+  file_ = fopen(file_name, mode);
+  UMASSERT(!file_, COULD_NOT_OPEN_FILE);
+}
+
+
+void shush::file::File::Write(const char* str) {
+  int bytes_written = _write(_fileno(file_),
+                             str, MAXIMUM_CHARS_PER_WRITE);
+
+  UMASSERT(strlen(str) == bytes_written, COULD_NOT_WRITE_TO_FILE);
+}
+
+
+void shush::file::File::Read(char* out_str, size_t count) {
+  VERIFIED
+  const size_t read_bytes = fread(out_str, sizeof(char), count, file_);
+
+  UMASSERT(read_bytes > 0 || count == 0, COULD_NOT_READ_FROM_FILE);
+}
+
+
+size_t shush::file::File::GetFileSize() {
+  VERIFIED
+  struct stat file_stat {};
+  fstat(_fileno(file_), &file_stat);
+
+  return file_stat.st_size;
+}
+
+
+void shush::file::File::Ok() {
+  MASSERT(!file_, LOST_ACCESS_TO_A_FILE); //
+  MASSERT(_fileno(file_) > 1, LOST_ACCESS_TO_A_FILE);
+}
+
+
+char* shush::file::File::GetDumpMessage(int error_code) {
+  strcpy(dump_msg_container, file_name_);
+  return dump_msg_container;
+}
+
+
+char* shush::file::File::GetErrorName(int error_code) {
+  switch (error_code) {
+    case COULD_NOT_OPEN_FILE : {
+      strcpy(error_name_container, "Could not open file with given name");
+      break;
+    }
+    case LOST_ACCESS_TO_A_FILE : {
+      strcpy(error_name_container, "Lost access to the file");
+      break;
+    }
+    case COULD_NOT_READ_FROM_FILE : {
+      strcpy(error_name_container, "Could not read from file");
+    }
+    case NO_FILE_NAME_GIVEN : {
+      strcpy(error_name_container, "No file name given");
+    }
+    case COULD_NOT_WRITE_TO_FILE : {
+      strcpy(error_name_container, "Could not write to the file");
+    }
+    default : {
+      strcpy(error_name_container, "Unknown error");
+    }
+  }
+
+  return error_name_container;
+}
